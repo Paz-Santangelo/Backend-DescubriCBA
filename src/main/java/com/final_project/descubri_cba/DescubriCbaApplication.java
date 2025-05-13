6de074b57b6a17e2cfd397a4908b87cda9765d00
Paz-Santangelo <paz_santangelo@hotmail.com>
2025-05-13 15:03:01 -0300

    subiendo proyecto descubriCBA con spring boot vacio y dependencias

---
+++ src/main/java/com/final_project/descubri_cba/DescubriCbaApplication.java
@@ -0,0 +1,13 @@
+package com.final_project.descubri_cba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescubriCbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DescubriCbaApplication.class, args);
+	}
+
+}
