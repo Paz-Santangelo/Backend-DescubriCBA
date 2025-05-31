bdf18b6e13cc4cb3613f6f60ae597c1a9bc412ff
Paz-Santangelo <paz_santangelo@hotmail.com>
2025-05-31 18:03:14 -0300

    Se han creado las clases abstractas Destiny e Image, asi como las clases ImageUser, ImageDestiny y User con sus respectivas anotaciones

--- src/main/java/com/final_project/descubri_cba/model/Image.java
+++ src/main/java/com/final_project/descubri_cba/model/Image.java
@@ -1,4 +1,29 @@
 package com.final_project.descubri_cba.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
 public abstract class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String imageUrl;
+    private String imageId;
+
+    public Image(String name, String imageUrl, String imageId) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.imageId = imageId;
+    }
 }
