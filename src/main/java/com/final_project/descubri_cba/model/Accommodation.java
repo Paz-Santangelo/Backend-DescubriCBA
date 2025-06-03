928d36bf7144b6d778541ceeacb5cff854908890
mroxanam <mroxanam20@gmail.com>
2025-06-03 17:24:46 -0300

    Modificaciones en enums y modelos: TypeBodyOfWater, TypeOfEmergency, Accommodation, BodyOfWater, EmergencyServices y Restaurant

--- src/main/java/com/final_project/descubri_cba/model/Accommodation.java
+++ src/main/java/com/final_project/descubri_cba/model/Accommodation.java
@@ -1,4 +1,24 @@
 package com.final_project.descubri_cba.model;
+import com.final_project.descubri_cba.enums.AccommodationType;
+import jakarta.persistence.*;
+import lombok.*;
 
-public class Accommodation {
+
+@Entity
+@Table(name = "accommodations")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Accommodation extends Destiny{
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AccommodationType type;
+
+
 }
+
+
