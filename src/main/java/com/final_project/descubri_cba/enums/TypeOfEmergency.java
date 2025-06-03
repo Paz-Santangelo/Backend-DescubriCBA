555cae6903d36f017de5626cde255a65b6733066
Paz-Santangelo <paz_santangelo@hotmail.com>
2025-06-03 18:30:09 -0300

    Se ha realizado la revision y correccion del enum TypeOfEmergency y de la clase EmergencyServices.

--- src/main/java/com/final_project/descubri_cba/enums/TypeOfEmergency.java
+++ src/main/java/com/final_project/descubri_cba/enums/TypeOfEmergency.java
@@ -1,8 +1,15 @@
 package com.final_project.descubri_cba.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum  TypeOfEmergency {
     SALUD,
     POLICIA,
     BOMBEROS,
-    AUXILIO_MECANICO
+    AUXILIO_MECANICO;
+
+    @JsonCreator
+    public static TypeOfEmergency fromString(String value) {
+        return value == null ? null : TypeOfEmergency.valueOf(value.toUpperCase());
+    }
 }
--- src/main/java/com/final_project/descubri_cba/model/EmergencyServices.java
+++ src/main/java/com/final_project/descubri_cba/model/EmergencyServices.java
@@ -1,21 +1,15 @@
 package com.final_project.descubri_cba.model;
+
 import com.final_project.descubri_cba.enums.TypeOfEmergency;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
 @Table(name = "emergency_services")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-
+@Entity
 public class EmergencyServices extends Destiny {
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "type_of_emergency", nullable = false)
     private TypeOfEmergency typeOfEmergency;
-
-
 }
