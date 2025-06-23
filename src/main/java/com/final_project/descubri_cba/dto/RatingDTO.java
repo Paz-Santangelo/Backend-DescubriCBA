ca3fd47488deb319a34da6183ab79b88dce86920
Paz-Santangelo <paz_santangelo@hotmail.com>
2025-06-23 20:02:16 -0300

    Correcciones en clases DTO de Image y Rating

--- src/main/java/com/final_project/descubri_cba/dto/RatingDTO.java
+++ src/main/java/com/final_project/descubri_cba/dto/RatingDTO.java
@@ -1,48 +1,19 @@
 package com.final_project.descubri_cba.dto;
+
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class RatingDTO {
-
-    @NotNull(message = "El nombre del destino no puede ser nulo")
-    @Size(min = 1, max = 100, message = "El nombre del destino debe tener entre 1 y 100 caracteres")
     private String nameDestination;
-
-    @Min(value = 1, message = "El puntaje mínimo es 1")
-    @Max(value = 5, message = "El puntaje máximo es 5")
     private int score;
-
-    public RatingDTO() {
-    }
-
-    public RatingDTO(String nameDestination, int score) {
-        this.nameDestination = nameDestination;
-        this.score = score;
-    }
-
-    public String getNameDestination() {
-        return nameDestination;
-    }
-
-    public void setNameDestination(String nameDestination) {
-        this.nameDestination = nameDestination;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
-
-    @Override
-    public String toString() {
-        return "RatingDTO{" +
-                "nameDestination='" + nameDestination + '\'' +
-                ", score=" + score +
-                '}';
-    }
 }
