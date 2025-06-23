9e4b3027f345baee01d97794190e0d165416dd14
Alejandro-Santangelo <artesaniaslarustica@gmail.com>
2025-06-23 18:42:30 -0300

    subiendo mis clases Dto

--- src/main/java/com/final_project/descubri_cba/dto/CommentDTO.java
+++ src/main/java/com/final_project/descubri_cba/dto/CommentDTO.java
@@ -1,4 +1,18 @@
 package com.final_project.descubri_cba.dto;
 
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CommentDTO {
+
+    private Long id;
+    private LocalDate date;
+    private String content;
+    private UserDTO user;
+
 }
--- src/main/java/com/final_project/descubri_cba/dto/UserDTO.java
+++ src/main/java/com/final_project/descubri_cba/dto/UserDTO.java
@@ -1,4 +1,25 @@
 package com.final_project.descubri_cba.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
+
+    private Long id;
+    private String email;
+    private String name;
+    private String lastname;
+    private String token;
+    private String tokenExpirationTime;
+    private String role;
+    private ImageDTO imageUser;
+    private List<CommentDTO> comments = new ArrayList<>();
+
 }
