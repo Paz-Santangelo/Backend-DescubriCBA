cab36257210d36f53d0647d915af394b3d631b79
Paz-Santangelo <paz_santangelo@hotmail.com>
2025-08-04 12:08:02 -0300

    Se han agregado las implementaciones de la interfaz JpaSpecificationExecutor a los repositorios para la funcionalidad de filtrado dinamico

--- src/main/java/com/final_project/descubri_cba/repository/IRestaurantRepository.java
+++ src/main/java/com/final_project/descubri_cba/repository/IRestaurantRepository.java
@@ -2,8 +2,9 @@ package com.final_project.descubri_cba.repository;
 
 import com.final_project.descubri_cba.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IRestaurantRepository extends JpaRepository<Restaurant, Long> {
+public interface IRestaurantRepository extends JpaRepository<Restaurant, Long>, JpaSpecificationExecutor<Restaurant> {
 }
