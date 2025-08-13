f11a4e771dfcd34a2982f95f3c4e64aa018acdcd
Paz-Santangelo <paz_santangelo@hotmail.com>
2025-08-13 12:10:18 -0300

    Se han realizado modificaciones en la clase Destination para agregar el atributo de user, de tipo User y dos metodos necesarios para gestionar la lista de imagenes de un destino.

--- src/main/java/com/final_project/descubri_cba/model/Destination.java
+++ src/main/java/com/final_project/descubri_cba/model/Destination.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Table(name = "destinations")
@@ -25,22 +26,46 @@ public abstract class Destination {
     private String department;
     private String locality;
     private String address;
+
     @Column(length = 1000)
     private String urlGoogleMaps;
+
     private String openingTime;
     private String closingTime;
     private Concurrence levelConcurrence;
     private boolean disabledAccessibility;
     private String numberPhone;
     private String cellPhone;
+
     @ElementCollection
     private List<String> website;
+
     @ElementCollection
     private List<String> paymentMethods;
+
+    @OneToMany(mappedBy = "destination", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<ImageDestination> imagesDestinations = new ArrayList<>();
+
     @OneToMany(mappedBy = "destination", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
-    private List<Rating> ratings;
+    private List<Rating> ratings = new ArrayList<>();
+
     @OneToMany(mappedBy = "destination", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Comment> comments;
+    private List<Comment> comments = new ArrayList<>();
+
     private int averageScore;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public void addImageDestination(ImageDestination image) {
+        this.imagesDestinations.add(image);
+        image.setDestination(this);
+    }
+
+    public void removeImageDestination(ImageDestination image) {
+        this.imagesDestinations.remove(image);
+        image.setDestination(null);
+    }
 }
