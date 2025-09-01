e807acc26662cee0308f42203c7643e483626b2b
Paz-Santangelo <paz_santangelo@hotmail.com>
2025-09-01 10:54:25 -0300

    Se crea el metodo login en AuthController.

--- src/main/java/com/final_project/descubri_cba/controller/AuthController.java
+++ src/main/java/com/final_project/descubri_cba/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.final_project.descubri_cba.controller;
 
+import com.final_project.descubri_cba.dto.LoginDTO;
+import com.final_project.descubri_cba.dto.UserDTO;
 import com.final_project.descubri_cba.model.User;
 import com.final_project.descubri_cba.service.IUserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,5 +23,11 @@ public class AuthController {
     public ResponseEntity<?> register(@Valid @RequestBody User user) {
         userService.register(user);
         return ResponseEntity.ok("Usuario registrado con éxito.");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginDTO loginDto) {
+        UserDTO userDTO = userService.login(loginDto);
+        return ResponseEntity.status(HttpStatus.OK).body(userDTO);
     }
 }
